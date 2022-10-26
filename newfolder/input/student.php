31630574c53f195299d5e1198aa9bba54d16697d
larsnotlars <lf.schuster1611@gmail.com>
2022-10-26 11:52:07 +0200

    test

--- newfolder/input/student.php
+++ newfolder/input/student.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Montserrat'
         rel='stylesheet'>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
     <title>Campus Connect</title>
 </head>
 
